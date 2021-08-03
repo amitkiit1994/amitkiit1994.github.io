1fd12e2bd5ba37b996a04ffb42f3db6a2e455f4d
Amit Kumar Das <amitkiit1994@gmail.com>
2021-08-03 21:00:06 +0530

    mail update

--- formValidateBootStrap.js
+++ formValidateBootStrap.js
@@ -51,28 +51,3 @@ $('#form').submit(function(e) {
 	$('#modal').modal('show');
 	e.preventDefault();
 });
-
-$('#form').submit(function(e)
-      {
-        e.preventDefault();
-
-        $form = $(this);
-        //show some response on the button
-        $('button[type="submit"]', $form).each(function()
-        {
-            $btn = $(this);
-            $btn.prop('type','button' );
-            $btn.prop('orig_label',$btn.text());
-            $btn.text('Sending ...');
-        });
-
-
-                    $.ajax({
-                type: "POST",
-                url: 'mailForm.php',
-                data: $form.serialize(),
-                success: after_form_submitted,
-                dataType: 'json'
-            });
-
-      });
